c9f52d57d15146668cccb4ef4f0589d81889ac23
Madusha Prasanjith <hi@madusha.me>
2022-12-22 03:19:03 +0100

    fix: Cleanup the network config.

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -11,28 +11,6 @@ const accounts = [
 
 const config: HardhatUserConfig = {
   solidity: '0.8.13',
-  networks: {
-    hardhat: {
-      chainId: 1337,
-      // allowUnlimitedContractSize: false,
-      // blockGasLimit: 20000000,
-      //   forking: {
-      //     url: "https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_MUMBAI}",
-      //     url: "https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_RINKEBY}",
-      //     url: "https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_MAINNET}",
-      //   },
-    },
-    mumbai: {
-      chainId: 80001,
-      url: process.env.RPC_80001 || 'https://rpc.ankr.com/polygon_mumbai',
-      accounts,
-    },
-    goerli: {
-      chainId: 5,
-      url: process.env.RPC_5 || 'https://rpc.ankr.com/eth_goerli',
-      accounts,
-    },
-  },
   namedAccounts: {
     deployer: {
       default: 0,
